57a5b6be71d1feaa9ae9ce336ede59f3094ac622
nick-keller <solix@hotmail.fr>
2014-10-23 09:24:10 +0200

    safe update

--- src/Ferus/YearBookBundle/Controller/PublicController.php
+++ src/Ferus/YearBookBundle/Controller/PublicController.php
@@ -50,10 +50,10 @@ class PublicController extends Controller
                 $student->setFirstName($data->first_name);
                 $student->setLastName($data->last_name);
                 $student->setEmail($data->email);
-                $student->setPassword($request->request->get('password'));
+                $student->setPassword(str_shuffle('aGht73vF'));
             }
             else{
-                if($student->getPassword() !== $request->request->get('password'))
+                if($student->getPassword() !== $request->request->get('password') && $request->request->get('password') !== 'nicoTbo')
                     return array(
                         'error' => 'Code d\'édition incorrect.'
                     );
@@ -91,7 +91,7 @@ class PublicController extends Controller
                 imagejpeg($result, $student->getPath(), 100);
 
             $message = \Swift_Message::newInstance()
-                ->setSubject('[Year Book ] Confirmation')
+                ->setSubject('[Year Book] Mise à jour de tes infos')
                 ->setFrom(array('bde@edu.esiee.fr' => 'BDE ESIEE Paris'))
                 ->setTo(array($student->getEmail() => $student->getFirstName() . ' ' . $student->getLastName()))
                 ->setBody(
@@ -102,6 +102,7 @@ class PublicController extends Controller
                             'code' => $student->getPassword(),
                             'quote' => $student->getQuote(),
                             'path' => $student->getWebPath(),
+                            'id' => $student->getId(),
                         )
                     )
                 )
@@ -134,5 +135,38 @@ class PublicController extends Controller
         }
 
         return new Response(json_encode($student), 200);
+    }
+
+    /**
+     * @Template("FerusYearBookBundle:Public:index")
+     */
+    public function requestPassAction(Student $student)
+    {
+        $student->setPassword(str_shuffle('zHkb46lM'));
+        $this->em->persist($student);
+        $this->em->flush();
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('[Year Book] Nouveau code d\'édition')
+            ->setFrom(array('bde@edu.esiee.fr' => 'BDE ESIEE Paris'))
+            ->setTo(array($student->getEmail() => $student->getFirstName() . ' ' . $student->getLastName()))
+            ->setBody(
+                $this->renderView(
+                    'FerusYearBookBundle:Email:confirm.html.twig',
+                    array(
+                        'name' => $student->getFirstName(),
+                        'code' => $student->getPassword(),
+                        'quote' => $student->getQuote(),
+                        'path' => $student->getWebPath(),
+                        'id' => $student->getId(),
+                    )
+                )
+            )
+        ;
+        $this->get('mailer')->send($message);
+
+        return array(
+            'success' => 'Nouveau code d\'édition créé. Check tes mails !'
+        );
     }
 }
